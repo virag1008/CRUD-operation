ad53784301e079152ed227aea5fcb5d7e66f4545
virag <viragjain010@gmail.com>
2023-03-02 19:11:32 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import {React_component ,Bike } from './react_component';
+// import Calc from "./Calc";
+// import Join from "./Join";
+// import If_Else from "./If_Else";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+// ********** CODE FOR REACT COMPONENT FILE ***************
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    {/* <React_component /> */}
+    {/* <Bike /> */}
+    {/* <Calc/> */}
+    {/* <Join/> */}
+    {/* <If_Else/>  */}
+    <App/>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
